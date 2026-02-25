8e649a811241f1a491ecff7aaf66b182dacb5082
Anton Baliasnikov <aba@matterlabs.dev>
2026-02-25 12:12:12 +0000

    chore: final version of update era

--- scripts/era/update_era.py
+++ scripts/era/update_era.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 import time
 from lib.script_context import ScriptCtx
@@ -7,6 +8,7 @@ from lib.entry import run_script
 from lib import utils
 from lib.protocol_version_era import PROTOCOL_TOOLCHAINS
 from lib import config
+from lib import edit_server
 
 
 def script(ctx: ScriptCtx) -> None:
@@ -14,6 +16,9 @@ def script(ctx: ScriptCtx) -> None:
     # Paths & constants
     # ------------------------------------------------------------------ #
     protocol_version: str = utils.require_env("PROTOCOL_VERSION", "v30")
+    prover_protocol_patch: str | None = None
+    if "PROVER_PROTOCOL_PATCH" in os.environ:
+        prover_protocol_patch = utils.require_env("PROVER_PROTOCOL_PATCH")
     try:
         toolchain = PROTOCOL_TOOLCHAINS[protocol_version]
     except KeyError:
@@ -37,67 +42,87 @@ def script(ctx: ScriptCtx) -> None:
             "cmake": ">=3",
             "gsutil": ">=5",
             "gh": ">=2",
+            "sqlx" : ">=0.8",
         }
     )
 
-    # bellman_cuda_dir = utils.prepare_bellman_cuda(ctx)
+    bellman_cuda_dir = utils.prepare_bellman_cuda(ctx)
 
     # ------------------------------------------------------------------ #
     # Build key generator
     # ------------------------------------------------------------------ #
-    # key_generator_path = (
-    #     ctx.repo_dir / "prover" / "target" / "release" / "key_generator"
-    # )
-    # key_generator_manifest_path = (
-    #     ctx.repo_dir
-    #     / "prover"
-    #     / "crates"
-    #     / "bin"
-    #     / "vk_setup_data_generator_server_fri"
-    #     / "Cargo.toml"
-    # )
-    # with ctx.section("Build key_generator binary", expected=120):
-    #     ctx.sh(
-    #         f"""
-    #         cargo build --features "gpu" --release \
-    #             --bin key_generator \
-    #             --manifest-path {key_generator_manifest_path}
-    #         """,
-    #         env={"BELLMAN_CUDA_DIR": str(bellman_cuda_dir)},
-    #     )
+    key_generator_path = (
+        ctx.repo_dir / "prover" / "target" / "release" / "key_generator"
+    )
+    key_generator_manifest_path = (
+        ctx.repo_dir
+        / "prover"
+        / "crates"
+        / "bin"
+        / "vk_setup_data_generator_server_fri"
+        / "Cargo.toml"
+    )
+    with ctx.section("Build key_generator binary", expected=120):
+        ctx.sh(
+            f"""
+            cargo build --features "gpu" --release \
+                --bin key_generator \
+                --manifest-path {key_generator_manifest_path}
+            """,
+            env={"BELLMAN_CUDA_DIR": str(bellman_cuda_dir)},
+        )
 
-    # # ------------------------------------------------------------------ #
-    # # Generate verification keys
-    # # ------------------------------------------------------------------ #
-    # with ctx.section("Generate verification keys", expected=300):
-    #     ctx.sh(f"{key_generator_path} generate-vk --path ./prover/data/keys")
+    # ------------------------------------------------------------------ #
+    # Generate verification keys
+    # ------------------------------------------------------------------ #
+    with ctx.section("Generate verification keys", expected=300):
+        ctx.sh(f"{key_generator_path} generate-vk --path ./prover/data/keys")
 
-    # # ------------------------------------------------------------------ #
-    # # Generate prover setup data
-    # # ------------------------------------------------------------------ #
-    # with ctx.section("Generate base layer setup data", expected=300):
-    #     for numeric_circuit in [*range(1, 20), 255]:
-    #         ctx.sh(
-    #             f"{key_generator_path} generate-sk-gpu basic --numeric-circuit {numeric_circuit}"
-    #         )
+    # ------------------------------------------------------------------ #
+    # Generate prover setup data
+    # ------------------------------------------------------------------ #
+    with ctx.section("Generate base layer setup data", expected=300):
+        for numeric_circuit in [*range(1, 20), 255]:
+            ctx.sh(
+                f"{key_generator_path} generate-sk-gpu basic --numeric-circuit {numeric_circuit}"
+            )
 
-    # with ctx.section("Generate recursive layer setup data", expected=300):
-    #     for numeric_circuit in [*range(1, 23), 255]:
-    #         ctx.sh(
-    #             f"{key_generator_path} generate-sk-gpu recursive --numeric-circuit {numeric_circuit}"
-    #         )
+    with ctx.section("Generate recursive layer setup data", expected=300):
+        for numeric_circuit in [*range(1, 23), 255]:
+            ctx.sh(
+                f"{key_generator_path} generate-sk-gpu recursive --numeric-circuit {numeric_circuit}"
+            )
 
-    # with ctx.section("Generate compressor data", expected=300):
-    #     crs_path = ctx.workspace / "setup.key"
-    #     utils.download(
-    #         config.CRS_FILE_URL,
-    #         crs_path,
-    #         checksum=config.CRS_FILE_SHA256_CHECKSUM,
-    #     )
-    #     ctx.sh(
-    #         f"{key_generator_path} generate-compressor-data",
-    #         env={"COMPACT_CRS_FILE": str(crs_path)},
-    #     )
+    with ctx.section("Generate compressor data", expected=300):
+        crs_path = ctx.workspace / "setup.key"
+        utils.download(
+            config.CRS_FILE_URL,
+            crs_path,
+            checksum=config.CRS_FILE_SHA256_CHECKSUM,
+        )
+        ctx.sh(
+            f"{key_generator_path} generate-compressor-data",
+            env={"COMPACT_CRS_FILE": str(crs_path)},
+        )
+
+    if prover_protocol_patch is not None:
+        with ctx.section("Update prover protocol patch", expected=2):
+            edit_server.update_rust_const(
+                ctx.repo_dir
+                / "prover"
+                / "crates"
+                / "lib"
+                / "prover_fri_types"
+                / "src"
+                / "lib.rs",
+                "PROVER_PROTOCOL_PATCH",
+                f"VersionPatch({int(prover_protocol_patch)})",
+                value_is_expr=True,
+            )
+    else:
+        ctx.logger.info(
+            "PROVER_PROTOCOL_PATCH is not set, skipping prover patch constant update."
+        )
 
     # ------------------------------------------------------------------ #
     # Generate json and upload data to GCP
@@ -118,114 +143,97 @@ def script(ctx: ScriptCtx) -> None:
         """
         setup_data_gpu_keys_json.write_text(json_content.strip())
 
-    # TODO: uncomment after testing
+    # TODO: uncomment before merge
     # with ctx.section("Upload data to GCP", expected=300):
     #     ctx.sh(f"gsutil -m rsync ./prover/data/keys {us}")
     #     ctx.sh(f"gsutil -m rsync -r {us} {asia}")
     #     ctx.sh(f"gsutil -m rsync -r {us} {europe}")
 
-    # with ctx.section("Update contracts with new verifier", expected=120):
-    #     # Copy the generated verification keys to the expected location for the contract generator
-    #     ctx.sh(
-    #         f"cp {ctx.repo_dir}/prover/data/keys/fflonk_verification_snark_key.json {ctx.repo_dir}/contracts/tools/data/fflonk_scheduler_key.json"
-    #     )
-    #     ctx.sh(
-    #         f"cp {ctx.repo_dir}/prover/data/keys/verification_snark_key.json {ctx.repo_dir}/contracts/tools/data/plonk_scheduler_key.json"
-    #     )
-    #     # Re-generate the verifier contracts with the new keys
-    #     ctx.sh(
-    #         f"""
-    #         cargo run --bin zksync_verifier_contract_generator --release -- \
-    #             --plonk_input_path {ctx.repo_dir}/contracts/tools/data/plonk_scheduler_key.json \
-    #             --fflonk_input_path {ctx.repo_dir}/contracts/tools/data/fflonk_scheduler_key.json \
-    #             --plonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L1VerifierPlonk.sol \
-    #             --fflonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L1VerifierFflonk.sol
-    #         """,
-    #         cwd=ctx.repo_dir / "contracts" / "tools",
-    #     )
-    #     # For L2 verifiers, we need to use the --l2_mode flag to generate the correct contracts
-    #     ctx.sh(
-    #         f"""
-    #         cargo run --bin zksync_verifier_contract_generator --release -- \
-    #             --l2_mode \
-    #             --plonk_input_path {ctx.repo_dir}/contracts/tools/data/plonk_scheduler_key.json \
-    #             --fflonk_input_path {ctx.repo_dir}/contracts/tools/data/fflonk_scheduler_key.json \
-    #             --plonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L2VerifierPlonk.sol \
-    #             --fflonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L2VerifierFflonk.sol
-    #         """,
-    #         cwd=ctx.repo_dir / "contracts" / "tools",
-    #     )
-    #     # Recompute hashes
-    #     ctx.sh(f"bash -c {ctx.repo_dir}/contracts/recompute_hashes.sh", cwd=ctx.repo_dir / "contracts")
+    with ctx.section("Update contracts with new verifier", expected=120):
+        # Copy the generated verification keys to the expected location for the contract generator
+        ctx.sh(
+            f"cp {ctx.repo_dir}/prover/data/keys/fflonk_verification_snark_key.json {ctx.repo_dir}/contracts/tools/data/fflonk_scheduler_key.json"
+        )
+        ctx.sh(
+            f"cp {ctx.repo_dir}/prover/data/keys/verification_snark_key.json {ctx.repo_dir}/contracts/tools/data/plonk_scheduler_key.json"
+        )
+        # Re-generate the verifier contracts with the new keys
+        ctx.sh(
+            f"""
+            cargo run --bin zksync_verifier_contract_generator --release -- \
+                --plonk_input_path {ctx.repo_dir}/contracts/tools/data/plonk_scheduler_key.json \
+                --fflonk_input_path {ctx.repo_dir}/contracts/tools/data/fflonk_scheduler_key.json \
+                --plonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L1VerifierPlonk.sol \
+                --fflonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L1VerifierFflonk.sol
+            """,
+            cwd=ctx.repo_dir / "contracts" / "tools",
+        )
+        # For L2 verifiers, we need to use the --l2_mode flag to generate the correct contracts
+        ctx.sh(
+            f"""
+            cargo run --bin zksync_verifier_contract_generator --release -- \
+                --l2_mode \
+                --plonk_input_path {ctx.repo_dir}/contracts/tools/data/plonk_scheduler_key.json \
+                --fflonk_input_path {ctx.repo_dir}/contracts/tools/data/fflonk_scheduler_key.json \
+                --plonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L2VerifierPlonk.sol \
+                --fflonk_output_path {ctx.repo_dir}/contracts/l1-contracts/contracts/state-transition/verifiers/L2VerifierFflonk.sol
+            """,
+            cwd=ctx.repo_dir / "contracts" / "tools",
+        )
+        # Recompute hashes
+        ctx.sh(f"bash -c {ctx.repo_dir}/contracts/recompute_hashes.sh", cwd=ctx.repo_dir / "contracts")
 
-    # with ctx.section("Commit and create PR for verifier contract changes", expected=30):
-    #     contracts_dir = ctx.repo_dir / "contracts"
-    #     branch = f"update-verifiers-{int(time.time())}"
+    with ctx.section("Commit and create PR for verifier contract changes", expected=30):
+        contracts_dir = ctx.repo_dir / "contracts"
+        branch = f"update-verifiers-{int(time.time())}"
 
-    #     has_changes = False
-    #     try:
-    #         ctx.sh(
-    #             ["git", "diff", "--exit-code", "--quiet"],
-    #             cwd=contracts_dir,
-    #             print_command=False,
-    #         )
-    #     except subprocess.CalledProcessError:
-    #         has_changes = True
+        has_changes = False
+        try:
+            ctx.sh(
+                ["git", "diff", "--exit-code", "--quiet"],
+                cwd=contracts_dir,
+                print_command=False,
+            )
+        except subprocess.CalledProcessError:
+            has_changes = True
 
-    #     if not has_changes:
-    #         ctx.logger.info(
-    #             "No verifier contract changes detected, skipping commit/PR."
-    #         )
-    #     else:
-    #         ctx.sh(
-    #             ["git", "config", "user.name", "zksync-admin-bot2"],
-    #             cwd=contracts_dir,
-    #         )
-    #         ctx.sh(
-    #             ["git", "config", "user.email", "temp-bot@matterlabs.dev"],
-    #             cwd=contracts_dir,
-    #         )
-    #         ctx.sh(["git", "checkout", "-b", branch], cwd=contracts_dir)
-    #         ctx.sh(
-    #             [
-    #                 "git",
-    #                 "add",
-    #                 "l1-contracts/contracts/state-transition/verifiers",
-    #                 "tools/data",
-    #                 "AllContractsHashes.json",
-    #             ],
-    #             cwd=contracts_dir,
-    #         )
-    #         ctx.sh(
-    #             [
-    #                 "git",
-    #                 "commit",
-    #                 "-m",
-    #                 "chore: update verifier contracts with new keys",
-    #             ],
-    #             cwd=contracts_dir,
-    #         )
-            # TODO: uncomment after testing
-            # ctx.sh(
-            #     ["git", "push", "--set-upstream", "origin", branch],
-            #     cwd=contracts_dir,
-            # )
-            # ctx.sh(
-            #     [
-            #         "gh",
-            #         "pr",
-            #         "create",
-            #         "--title",
-            #         "chore: update verifier contracts with new keys",
-            #         "--body",
-            #         "Update verifier contracts with new keys",
-            #         "--base",
-            #         "main",
-            #         "--head",
-            #         branch,
-            #     ],
-            #     cwd=contracts_dir,
-            # )
+        if not has_changes:
+            ctx.logger.info(
+                "No verifier contract changes detected, skipping commit/PR."
+            )
+        else:
+            ctx.sh(
+                ["git", "config", "user.name", "zksync-admin-bot2"],
+                cwd=contracts_dir,
+            )
+            ctx.sh(
+                ["git", "config", "user.email", "temp-bot@matterlabs.dev"],
+                cwd=contracts_dir,
+            )
+            ctx.sh(["git", "checkout", "-b", branch], cwd=contracts_dir)
+            ctx.sh(
+                [
+                    "git",
+                    "add",
+                    "l1-contracts/contracts/state-transition/verifiers",
+                    "tools/data",
+                    "AllContractsHashes.json",
+                ],
+                cwd=contracts_dir,
+            )
+            ctx.sh(
+                [
+                    "git",
+                    "commit",
+                    "-m",
+                    "chore: update verifier contracts with new keys",
+                ],
+                cwd=contracts_dir,
+            )
+
+    # ------------------------------------------------------------------ #
+    # Regenerate genesis
+    # ------------------------------------------------------------------ #
 
     with ctx.section("Build zkstack binary", expected=120):
         ctx.sh(
